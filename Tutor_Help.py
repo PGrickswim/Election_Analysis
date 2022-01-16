40dcf1c90ba1fb22182f285c774375c03f5d3beb
Richard.Sadowski <Richard.Sadowski@target.com>
2022-01-16 06:34:59 -0600

    Adding PyPoll.py and others here

---
+++ Tutor_Help.py
@@ -0,0 +1,39 @@
+# The data we need to retrieve
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won 
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote
+# Import the datetime class from the datetime module.
+# import datetime as dt
+# # Use the now() attribute on the datetime class to get the present time.
+# now = dt.datetime.now()
+# # Print the present time.
+# print("The time right now is ", now)
+# import csv
+# dir(csv)
+
+# x = [{"fn": "D", "ln": 100, "fl":[1,2,3]}, {"fn": "a"}, {"fn": "b"}]
+
+# y = x[0]["fl"][2]
+# print(y)
+
+# z = [1, '2']
+# y = z[1]
+
+# w = 100 + y
+# print(w)
+
+# for i in [1,2,3]:
+#     print(i) 
+
+#     for j in [4,5,6,7]:
+#         if j == 5:
+#             print("It is 5")
+#         elif j == 6:
+#             print("it is 6") 
+#         else:
+#             print(j)  
+
+
+  
